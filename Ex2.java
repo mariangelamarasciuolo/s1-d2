9d95d7b463a1a9fdcd2597c8f49cf91d666a7af2
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-10-03 15:41:27 +0200

    Esercizio 2
    Esercizio 2 terminato

---
+++ Ex2.java
@@ -0,0 +1,26 @@
+public class Ex2 {
+    public static void main (String[] args) {
+    int n = 4;
+    switch (n) {
+        case 0: {
+            System.out.println("ZERO");
+            break;
+        }
+        case 1: {
+            System.out.println("UNO");
+            break;
+        }
+        case 2: {
+            System.out.println("DUE");
+            break;
+        }
+        case 3: {
+            System.out.println("TRE");
+            break;
+        }
+        default: {
+            System.out.println("ERRORE");
+        }
+    }
+    }
+}
